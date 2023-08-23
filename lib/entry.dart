d8853354f0d73e2e1ad7353054eba7bd80599083
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-08-23 16:13:23 +0900

    add entry class for firestore

---
+++ lib/entry.dart
@@ -0,0 +1,23 @@
+class Entry {
+  final DateTime date;
+  final String country;
+  final String producer;
+  final String roastLevel;
+  final String mesh;
+  final String processing;
+  final String variety;
+  final String extracting;
+  final String comment;
+
+  Entry({
+    required this.date,
+    required this.country,
+    required this.producer,
+    required this.roastLevel,
+    required this.mesh,
+    required this.processing,
+    required this.variety,
+    required this.extracting,
+    required this.comment,
+  });
+}
